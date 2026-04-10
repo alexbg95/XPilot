c195064f131b969ffaa217f4d0315646107aefa0
Alex Benitez <3alexdark@gmail.com>
2026-04-09 23:23:10 -0500

    fix: corregir import duplicado en usuarioRepository

--- src/main/java/com/XPilot/XPilot/repositories/usuarioRepository.java
+++ src/main/java/com/XPilot/XPilot/repositories/usuarioRepository.java
@@ -6,16 +6,11 @@ import org.springframework.stereotype.Repository;
 import com.XPilot.XPilot.models.usuario;
 
 @Repository
-import java.util.Optional;
 public interface usuarioRepository extends JpaRepository<usuario, Long> {
-
     Optional<usuario> findByEmail(String email);
-
     Optional<usuario> findByUname(String uname);
-
     boolean existsByEmail(String email);
     boolean existsByUname(String uname);
     Optional<usuario> findByTokenVerificacion(String token);
-
     usuario findByRol(String rol);
 }
